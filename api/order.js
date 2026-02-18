3a084c53352eba16e0965f59a05fa5db20ac82bb
wakassharif92 <wakas.sharif92@gmail.com>
2026-02-18 17:40:29 +0500

    change

--- api/order.js
+++ api/order.js
@@ -56,6 +56,14 @@ async function sendWhatsAppMessage(text) {
 }
 
 module.exports = async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
   }
